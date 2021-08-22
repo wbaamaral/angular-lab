309618deee43b5310b41924d9d2a10998e10babd
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-08-22 18:27:50 -0400

    Eventos angular - Utilizando sitaxe sugar -  003

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -7,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   nome = 'Thiago';
+
   constructor() { }
 
   ngOnInit(): void {
   }
   adicionar(){
     console.log(`Adicionando ${this.nome}`);
+    const numero = Math.round(Math.random() * 100);
+    this.nome = 'João' + numero;
+  }
+
+  alterarNome( evento: any){
+    /*
+    * console.log(evento)
+    * lista todas as propriedades do evento.
+    * dentro dela vamos pegar target e value
+    * para fazer a atualização da variável.
+    *
+    */
+    this.nome = evento.target.value;
   }
 }
