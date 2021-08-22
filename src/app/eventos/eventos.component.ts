1fc32e12c6662606cdebd5cf2b654611a9337622
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-08-22 18:47:01 -0400

    Eventos angular - Utilizar variável de referencia - 005

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -6,26 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  nome = 'Thiago';
+  nome = 'Amaral';
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  adicionar(){
-    console.log(`Adicionando ${this.nome}`);
-    const numero = Math.round(Math.random() * 100);
-    this.nome = 'João' + numero;
-  }
 
-  alterarNome( evento: any){
-    /*
-    * console.log(evento)
-    * lista todas as propriedades do evento.
-    * dentro dela vamos pegar target e value
-    * para fazer a atualização da variável.
-    *
-    */
-    this.nome = evento.target.value;
+  alterarNome( parametro: string){
+     this.nome = parametro;
   }
 }
