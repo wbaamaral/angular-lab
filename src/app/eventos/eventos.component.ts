0e1c4b8f6bb3eb934d142f15245de50f2f4f9e86
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-08-22 22:37:28 -0400

    Iterando com a diretiva ngFor

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit, NgModule } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent {
+
+  ultimoId = 0;
   nome = 'Amaral';
+
+
+  // funcionarios = [] as any;
+  funcionarios: Array<any> = [];
+
   adicionado = false;
 
   adicionar() {
     console.log(`Adicionando ${this.nome}`);
     this.adicionado = true;
+
+    this.funcionarios.push({
+      id: ++this.ultimoId,
+      nome: this.nome
+    });
   }
 }
